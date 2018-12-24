a67e08baeab2e827ba14eafca42c8bacce0630fa
jellyfish77 <jellyfish77@gmx.com>
2018-12-24 21:46:17 +0800

    Add minor changes to ch09 classes

---
+++ src/ch09/interfaces/defaut_methods/MyIFStaticTest.java
@@ -0,0 +1,11 @@
+package ch09.interfaces.defaut_methods;
+
+public class MyIFStaticTest {
+		
+	public static void main(String args[]) {
+	
+		
+		System.out.println(MyIF.getDefaultNumber());
+	
+	}
+}
