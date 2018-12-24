09503cfa0236e71722e87550c89bef191d5889c3
jellyfish77 <someone@blah.com>
2018-12-24 15:21:59 +0800

    Add classes to ch09.interfaces.extend

---
+++ src/ch09/interfaces/extended/A.java
@@ -0,0 +1,8 @@
+package ch09.interfaces.extended;
+
+//One interface an extend another.
+interface A {
+	void meth1();
+
+	void meth2();
+}
---
+++ src/ch09/interfaces/extended/IFExtend.java
@@ -0,0 +1,11 @@
+package ch09.interfaces.extended;
+
+class IFExtend {
+	public static void main(String arg[]) {
+		MyClass ob = new MyClass();
+
+		ob.meth1();
+		ob.meth2();
+		ob.meth3();
+	}
+}
