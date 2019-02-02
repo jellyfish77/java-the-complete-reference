10e528a825e283f2073920e816d7c1ea4ed64c57
jellyfish77 <jellyfish77@gmx.com>
2019-02-02 11:32:26 +0800

    Add thread demo classes

---
+++ src/ch11/threads/multithread/MultiThreadDemo.java
@@ -0,0 +1,18 @@
+package ch11.threads.multithread;
+
+class MultiThreadDemo {
+	public static void main(String args[]) {
+		new NewThread("One"); // start threads
+		new NewThread("Two");
+		new NewThread("Three");
+
+		try {
+			// wait for other threads to end
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			System.out.println("Main thread Interrupted");
+		}
+
+		System.out.println("Main thread exiting.");
+	}
+}
