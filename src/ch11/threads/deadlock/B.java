9d7e5873486d697e1ae31ab1589f3d1f1c500e5f
jellyfish77 <jellyfish77@gmx.com>
2019-02-03 23:14:19 +0800

    Add thread examples

---
+++ src/ch11/threads/deadlock/B.java
@@ -0,0 +1,21 @@
+package ch11.threads.deadlock;
+
+class B {
+	synchronized void bar(A a) {
+		String name = Thread.currentThread().getName();
+		System.out.println(name + " entered B.bar");
+
+		try {
+			Thread.sleep(1000);
+		} catch (Exception e) {
+			System.out.println("B Interrupted");
+		}
+
+		System.out.println(name + " trying to call A.last()");
+		a.last();
+	}
+
+	synchronized void last() {
+		System.out.println("Inside A.last");
+	}
+}
---
+++ src/ch11/threads/deadlock/DeadLock.java
@@ -0,0 +1,24 @@
+package ch11.threads.deadlock;
+
+class Deadlock implements Runnable {
+	A a = new A();
+	B b = new B();
+
+	Deadlock() {
+		Thread.currentThread().setName("MainThread");
+		Thread t = new Thread(this, "RacingThread");
+		t.start();
+
+		a.foo(b); // get lock on a in this thread.
+		System.out.println("Back in main thread");
+	}
+
+	public void run() {
+		b.bar(a); // get lock on b in other thread.
+		System.out.println("Back in other thread");
+	}
+
+	public static void main(String args[]) {
+		new Deadlock();
+	}
+}
